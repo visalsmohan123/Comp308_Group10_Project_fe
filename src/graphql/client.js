49d276386c286d680f9c514e83ce80138caa1acd
Visal Mohan <vmohan3@my.centennialcollege.ca>
2024-04-18 19:13:01 -0400

    updated backend url

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink, ApolloProvider } from '@apollo/client';
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:4000/graphql', // Adjust if your GraphQL server is on a different URL
+  uri: 'https://comp308-be.azurewebsites.net/graphql', // Adjust if your GraphQL server is on a different URL
 });
 
 const client = new ApolloClient({
